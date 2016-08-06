bde067d81250f3cad8be355dfb8b21fce4b8e3d9
ug <suzaku622@gmail.com>
2016-08-06 21:54:46 +0900

    VIEWポートを追加した

--- app/assets/javascripts/application.js
+++
@@ -1,4 +0,0 @@
-//= require jquery
-//= require jquery_ujs
-//= require materialize-sprockets
-//= require_tree .
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'about/index'
+
 	root "home#index"
 	resources :about, only: :index
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
