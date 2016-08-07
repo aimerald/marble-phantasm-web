789b5633676fc8c7d7ec5f7bb7c121442bf790bc
ug <suzaku622@gmail.com>
2016-08-08 00:13:00 +0900

    Blog::body string to text

--- db/migrate/20160805031634_create_blogs.rb
+++ db/migrate/20160805031634_create_blogs.rb
@@ -2,7 +2,7 @@ class CreateBlogs < ActiveRecord::Migration[5.0]
   def change
     create_table :blogs do |t|
       t.string :title, null: false, default: ""
-      t.string :body, null: false, default: ""
+      t.text :body
       t.string :image
       t.references :user, foreign_key: true
 
