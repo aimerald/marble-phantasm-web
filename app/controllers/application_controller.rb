2ce1a774204f3eff5c1f1f3a8baa31c19bb62453
ug <suzaku622@gmail.com>
2016-08-07 16:18:38 +0900

    ブラーの修正とタイトルの変数化

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,18 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
-  
+  protect_from_forgery with: :exception  
   before_action :set_sky
+	before_action :set_blur
+	before_action :set_title
+
+	def set_blur
+		@blur = ""
+	end
+
+	def set_title
+		@title = "Season'sCall:::ハッカーを目指す青年のポートフォリオ"
+	end
 
   def set_sky
     @sky = "night_sky"
