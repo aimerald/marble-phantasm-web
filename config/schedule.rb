120d230af8f53739f6205d64b13056b367be210b
UG <suzaku622@gmail.com>
2016-12-02 15:13:05 +0900

    Wheneverのログ

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,21 +1,12 @@
+# coding: utf-8
 # Use this file to easily define all of your cron jobs.
 #
 # It's helpful, but not entirely necessary to understand cron before proceeding.
 # http://en.wikipedia.org/wiki/Cron
 
 # Example:
-#
-# set :output, "/path/to/my/cron_log.log"
-#
-# every 2.hours do
-#   command "/usr/bin/some_great_command"
-#   runner "MyModel.some_method"
-#   rake "some:great:rake:task"
-# end
-#
-# every 4.days do
-#   runner "AnotherModel.prune_old_records"
-# end
+# ログの吐出口
+set :output, "log/cron.log"
 
 # Learn more: http://github.com/javan/whenever
 every 66.days do
