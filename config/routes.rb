f3a6f7947e5e49b59b6258cab2aa456b75bd5769
aimerald <aimerald3@gmail.com>
2017-12-29 22:36:40 +0900

    remove unncessary routings

--- config/routes.rb
+++ config/routes.rb
@@ -5,20 +5,8 @@ Rails.application.routes.draw do
       post :result
     end
   end
-  
-  resources :blogs
 
-  resources :users do
-		collection do
-			get :sign_in
-			#get :sign_up
-			
-			post :sign_in_process
-			
-			delete :sign_out
-		end
-	end
+  resources :blogs
 	root "home#index"
 	resources :about, only: :index
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
