7c6948526156af1bf60fcb70433ab8801c5f030f
aimerald <aimerald3@gmail.com>
2018-01-21 01:45:38 +0900

    remove unuse routing

--- api_server/config/routes.rb
+++ api_server/config/routes.rb
@@ -1,12 +1,4 @@
 Rails.application.routes.draw do
-
-  resources :searches, expect: [:new,:edit] do
-    collection do
-      post :result
-    end
-  end
-
-  resources :blogs
-	root "home#index"
-	resources :about, only: :index
+  resources :about, only: :index
+  root 'home#index'
 end
